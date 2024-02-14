eaa2ab5013883a9a654203b355909f709fb5a647
NunezJayson <nunez.jayson@mscmarinduque.edu.ph>
2024-02-14 23:51:49 +0800

    My Educational Background

--- git-python-nunez.py
+++ git-python-nunez.py
@@ -2,3 +2,8 @@
 print("Jayson Nuñez")
 print("I am From Balanacan Mogpog Marinduque")
 print("May 26, 2001")
+
+# My Educational Background
+print("Marinduque States College")
+print("BS Information Tech")
+print("2nd year college")
